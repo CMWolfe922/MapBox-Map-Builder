03785be856db8af41f4d38e9629b849196473456
cmwolfe <79966004+CMWolfe922@users.noreply.github.com>
2023-01-29 21:53:55 -0600

    updated the apps file.

--- mapboxsite/settings.py
+++ mapboxsite/settings.py
@@ -134,3 +134,6 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'index.html'
+LOGIN_REDIRECT_URL = 'home.html'
